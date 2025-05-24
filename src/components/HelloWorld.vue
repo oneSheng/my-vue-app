e2fc27227c4cdc337ac5c41eff9daec005acea51
yangsai <15864240+yangsai666@user.noreply.gitee.com>
2025-05-24 22:10:44 +0800

    feat: 修改页面文案

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,8 +4,9 @@
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit1
-      <code>components/HelloWorld.vue</code> to test HMR
+      <!-- Edit1
+      <code>components/HelloWorld.vue</code> to test HMR -->
+      刘朋利杨赛
     </p>
   </div>
 
