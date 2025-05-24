8b5770eb614b3ea1e3e5140f23d564fb2c5bfb71
yangsai <15864240+yangsai666@user.noreply.gitee.com>
2025-05-24 14:56:23 +0800

    feat: 调整代码位置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,10 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
+      Edit1
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
@@ -35,6 +25,15 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
 
 <style scoped>
 .read-the-docs {
